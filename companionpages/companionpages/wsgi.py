93e416b84002c45f6f762d88d52c5f4cc1f414e1
codersquid <github@codersquid.com>
2013-07-01 17:48:17 -0500

    hello world mucking about

--- companionpages/companionpages/wsgi.py
+++ companionpages/companionpages/wsgi.py
@@ -1,16 +1,16 @@
 """
 WSGI config for companionpages project.
 
-This module contains the WSGI application used by Django's development
-server and any production WSGI deployments. It should expose a module-level
-variable named ``application``. Django's ``runserver`` and ``runfcgi``
-commands discover this application via the ``WSGI_APPLICATION`` setting.
+This module contains the WSGI application used by Django's development server
+and any production WSGI deployments. It should expose a module-level variable
+named ``application``. Django's ``runserver`` and ``runfcgi`` commands discover
+this application via the ``WSGI_APPLICATION`` setting.
 
 Usually you will have the standard Django WSGI application here, but it also
-might make sense to replace the whole Django WSGI application with a custom
-one that later delegates to the Django one. For example, you could introduce
-WSGI middleware here, or combine a Django application with an application of
-another framework.
+might make sense to replace the whole Django WSGI application with a custom one
+that later delegates to the Django one. For example, you could introduce WSGI
+middleware here, or combine a Django application with an application of another
+framework.
 
 """
 import os
---
+++ companionpages/profiles/views.py
@@ -0,0 +1,5 @@
+# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+        return HttpResponse("Hello, profiles!")
