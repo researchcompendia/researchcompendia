3d24b94018e32a6c6792630edb7ea82c1be758b3
codersquid <github@codersquid.com>
2013-07-01 23:19:16 -0500

    this time I actually grabbed the generated 2scoops files, doh

--- companionpages/companionpages/urls.py
+++ companionpages/companionpages/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', TemplateView.as_view(template_name='base.html')),
     # Examples:
     #url(r'^$', 'companionpages.views.home', name='home'),
     url(r'^profiles/', include('profiles.urls')),
