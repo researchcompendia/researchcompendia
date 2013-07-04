f7d95c06c9374c509c84573157e0e6d599ca603b
codersquid <sheila@codersquid.com>
2013-07-04 17:56:48 +0000

    playing with settings

--- companionpages/companionpages/settings/local.py
+++ companionpages/companionpages/settings/local.py
@@ -25,12 +25,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': normpath(join(DJANGO_ROOT, 'default.db')),
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tyler',
     }
 }
 ########## END DATABASE CONFIGURATION
--- companionpages/companionpages/settings/production.py
+++ companionpages/companionpages/settings/production.py
@@ -5,19 +5,6 @@ from os import environ
 
 from base import *
 
-# Normally you should not import ANYTHING from Django directly
-# into your settings, but ImproperlyConfigured is an exception.
-from django.core.exceptions import ImproperlyConfigured
-
-
-def get_env_setting(setting):
-    """ Get the environment setting or return exception """
-    try:
-        return environ[setting]
-    except KeyError:
-        error_msg = "Set the %s env variable" % setting
-        raise ImproperlyConfigured(error_msg)
-
 ########## HOST CONFIGURATION
 # See: https://docs.djangoproject.com/en/1.5/releases/1.5/#allowed-hosts-required-in-production
 ALLOWED_HOSTS = []
