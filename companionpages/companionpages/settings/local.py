f4572cb7931be006b1ff76b3f571c79108c631b3
codersquid <github@codersquid.com>
2013-07-09 15:04:25 -0500

    found out about the lorem tag

--- companionpages/companionpages/settings/local.py
+++ companionpages/companionpages/settings/local.py
@@ -48,6 +48,7 @@ CACHES = {
 # See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
 INSTALLED_APPS += (
     'debug_toolbar',
+    'django.contrib.webdesign',
 )
 
 # See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
