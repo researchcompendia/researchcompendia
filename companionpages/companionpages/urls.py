b69c7d2905a912ed7c161943b42f9b579a71a87c
codersquid <github@codersquid.com>
2013-07-08 21:14:33 -0500

    removing home and sites

--- companionpages/companionpages/urls.py
+++ companionpages/companionpages/urls.py
@@ -7,14 +7,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='base.html')),
-    # Examples:
-    #url(r'^$', 'companionpages.views.home', name='home'),
-    url(r'^profiles/', include('profiles.urls')),
-    url(r'^home/', include('home.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+    #url(r'^profiles/', include('profiles.urls')),
+    url(r'^accounts/', include('registration.backends.default.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
