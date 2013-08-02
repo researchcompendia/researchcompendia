cc2d975fbc38f9cabea2a5f176568dd7ca628dbf
codersquid <github@codersquid.com>
2013-08-01 22:56:50 -0500

    adding new empty app

--- companionpages/companionpages/settings.py
+++ companionpages/companionpages/settings.py
@@ -178,6 +178,7 @@ THIRD_PARTY_APPS = (
 
 # Apps specific for this project go here.
 LOCAL_APPS = (
+    'results',
 )
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
