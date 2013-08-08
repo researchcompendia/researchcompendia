18c3435930137089e38c4b06994326c470db50e8
codersquid <github@codersquid.com>
2013-08-07 19:07:12 -0500

    customizing contact form

--- companionpages/companionpages/urls.py
+++ companionpages/companionpages/urls.py
@@ -4,11 +4,14 @@ from django.views.generic import TemplateView
 from django.contrib import admin
 admin.autodiscover()
 
+from envelope.views import ContactView
+
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='base.html')),
     url(r'^profiles/', include('profiles.urls')),
     url(r'^accounts/', include('registration.backends.default.urls')),
-    url(r'^contact/', include('envelope.urls')),
+    url(r'^contact/', ContactView.as_view(template_name='contact.html'),
+        name='envelope-contact'),
     #url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
