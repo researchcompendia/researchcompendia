e7575a1b66d293f3760d4a6dd914af2d8ed20821
codersquid <github@codersquid.com>
2013-08-14 12:58:15 -0500

    south migration files

--- companionpages/companionpages/settings.py
+++ companionpages/companionpages/settings.py
@@ -181,6 +181,7 @@ THIRD_PARTY_APPS = (
 LOCAL_APPS = (
     'members',
     'news',
+    #'supportingmaterials',
 )
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
