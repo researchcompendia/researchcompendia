83057e634750c33a71f9eb7cac13d8aa4eb3c509
codersquid <github@codersquid.com>
2013-08-14 15:23:28 -0500

    supportingmaterials model confusion

--- companionpages/supportingmaterials/models.py
+++ companionpages/supportingmaterials/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
-from companionpages.members import Member
+#from companionpages.members import Member
 
 
 class CompanionArticle(models.Model):
-    corresponding_author = models.OneToOneField(members.Member, help_text=u'The primary point of contact')
-    collaborators = models.ManyToManyField(members.Member, blank=True, help_text=u'Co-authors and collaborators')
+    #corresponding_author = models.OneToOneField(members.Member, help_text=u'The primary point of contact')
+    #collaborators = models.ManyToManyField(members.Member, blank=True, help_text=u'Co-authors and collaborators')
     title = models.CharField(max_length=100, help_text=u'Title of the publication')
     abstract = models.TextField(max_length=500)
     document = models.FileField(upload_to='papers', blank=True)
@@ -19,6 +19,6 @@ class SupportingMaterial(models.Model):
     name = models.CharField(max_length=100)
     archive_file = models.FileField(upload_to='materials', blank=True)
     explanatory_text = models.TextField(max_length=1000, blank=True)
-    materials_url = models.URLField(blank=True, blank=True, help_text=u'URL to the supporting material. For example, '
-                                                                      u'if this is source code, this would be a url '
-                                                                      u'to to the code repository.')
+    materials_url = models.URLField(blank=True, help_text=u'URL to the supporting material. For example, '
+                                                          u'if this is source code, this would be a url '
+                                                          u'to to the code repository.')
