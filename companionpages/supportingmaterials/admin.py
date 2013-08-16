ebf0c47399e67e5eeedd73461b87104dc8587c5e
codersquid <github@codersquid.com>
2013-08-16 15:52:22 -0500

    inline editing on admin page for supporting materials

--- companionpages/supportingmaterials/admin.py
+++ companionpages/supportingmaterials/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from .models import CompanionArticle, SupportingMaterial
 
+class SupportingInline(admin.StackedInline):
+    model = SupportingMaterial
+
+
 class CompanionArticleAdmin(admin.ModelAdmin):
     date_heirarchy = ['created']
     prepopulated_fields = {'slug': ('title',)}
+    inlines = [SupportingInline]
+
 
 class SupportingMaterialAdmin(admin.ModelAdmin):
     date_heirarchy = ['created']
