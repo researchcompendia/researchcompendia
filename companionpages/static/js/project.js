fb0b3fa1362054ecb4c9428ca1b07aa084b685e2
codersquid <github@codersquid.com>
2013-08-20 15:12:21 -0500

    corrected icon toggle javascript

--- companionpages/static/js/project.js
+++ companionpages/static/js/project.js
@@ -1,13 +1,14 @@
-/* this is broken. need help */
+/* 
+ * this toggles the + icons to - icons when a FAQ question is
+ * collapsed, and visa versa
+ */
 $('.panel').on("hidden.bs.collapse", function(e) {
-  $icon = $( "div#iconstate" )
-  console.log('hidden', $icon)
+  $icon = $( e.currentTarget ).find( "div#iconstate" )
   $icon.removeClass("icon-plus-sign-alt")
   $icon.addClass("icon-minus-sign-alt")
 });
 $('.panel').on("shown.bs.collapse", function(e) {
-  $icon = $( "div#iconstate" )
-  console.log('shown', $icon)
+  $icon = $( e.currentTarget ).find( "div#iconstate" )
   $icon.removeClass("icon-minus-sign-alt")
   $icon.addClass("icon-plus-sign-alt")
 });
