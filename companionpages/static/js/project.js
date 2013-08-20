3fbe46158ad8c4bc26b0e3b79276c34f011bbe30
codersquid <github@codersquid.com>
2013-08-20 14:34:43 -0500

    need help with font toggle

--- companionpages/static/js/project.js
+++ companionpages/static/js/project.js
@@ -1,1 +1,13 @@
-/* Project specific Javascript goes here. */
+/* this is broken. need help */
+$('.panel').on("hidden.bs.collapse", function(e) {
+  $icon = $( "div#iconstate" )
+  console.log('hidden', $icon)
+  $icon.removeClass("icon-plus-sign-alt")
+  $icon.addClass("icon-minus-sign-alt")
+});
+$('.panel').on("shown.bs.collapse", function(e) {
+  $icon = $( "div#iconstate" )
+  console.log('shown', $icon)
+  $icon.removeClass("icon-minus-sign-alt")
+  $icon.addClass("icon-plus-sign-alt")
+});
