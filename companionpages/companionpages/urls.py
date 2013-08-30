e5fd38fb0318b08a53ea3bf72355aefe0912ca4e
codersquid <github@codersquid.com>
2013-08-29 22:45:45 -0500

    removed avatar urls

--- companionpages/companionpages/urls.py
+++ companionpages/companionpages/urls.py
@@ -22,7 +22,6 @@ urlpatterns = patterns('',
     url(r'^news/', include('news.urls')),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^avatar/', include('avatar.urls')),
 )
 
 urlpatterns += patterns('django.contrib.flatpages.views',
