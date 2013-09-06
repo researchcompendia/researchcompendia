ba68fe368343534a36882db84725174afadee1ec
codersquid <github@codersquid.com>
2013-09-05 20:44:44 -0500

    increased size of fields for supporting materials, made tags optional

--- companionpages/supportingmaterials/models.py
+++ companionpages/supportingmaterials/models.py
@@ -12,12 +12,14 @@ class CompanionArticle(StatusModel, TimeStampedModel):
     corresponding_author = models.ForeignKey(Member, help_text=_(u'The primary point of contact'))
     # how to represent collaborators? Members? but don't want them to be required to be
     STATUS = Choices('active', 'inactive')
-    title = models.CharField(max_length=100, help_text=_(u'Title of the publication'))
-    abstract = models.TextField(max_length=500)
+    title = models.CharField(max_length=500, help_text=_(u'Title of the publication'))
+    abstract = models.TextField(max_length=5000)
     document = models.FileField(upload_to='papers', blank=True)
+    journal = models.CharField(blank=True, max_length=500, help_text=_(u'Journal Name'))
     article_url = models.URLField(blank=True, help_text=_(u'URL to the paper.'))
     slug = models.SlugField(unique=True)
-    tags = TaggableManager()
+    tags = TaggableManager(blank=True)
+    legacy_id = models.IntegerField(blank=True, null=True)
 
     def __unicode__(self):
         return self.title
@@ -26,13 +28,13 @@ class CompanionArticle(StatusModel, TimeStampedModel):
 class SupportingMaterial(StatusModel, TimeStampedModel):
     STATUS = Choices('active', 'inactive')
     companion_article = models.ForeignKey(CompanionArticle)
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=500)
     archive_file = models.FileField(upload_to='materials', blank=True)
-    explanatory_text = models.TextField(max_length=1000, blank=True)
+    explanatory_text = models.TextField(max_length=5000, blank=True)
     materials_url = models.URLField(blank=True, help_text=_(u'URL to the supporting material. For example, '
                                                             u'if this is source code, this would be a url '
                                                             u'to to the code repository.'))
-    tags = TaggableManager()
+    tags = TaggableManager(blank=True)
 
     def __unicode__(self):
         return self.name
