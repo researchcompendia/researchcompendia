18d482a119d651260cbaeb1459f431d16bcfde24
codersquid <github@codersquid.com>
2013-10-24 12:05:40 -0500

    added make_active action to admin so that compendia can be changed to active from draft in the admin

--- companionpages/supportingmaterials/admin.py
+++ companionpages/supportingmaterials/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from .models import Collaborator, SupportingMaterial, Article
 
 
+def make_active(modeladmin, request, queryset):
+    queryset.update(status=Article.STATUS.active)
+make_active.short_description = "Mark selected item as active"
+
+
 class SupportingInline(admin.StackedInline):
     model = SupportingMaterial
 
@@ -12,15 +17,20 @@ class CollaboratorInline(admin.StackedInline):
 
 class ArticleAdmin(admin.ModelAdmin):
     date_heirarchy = ['created']
+    list_filter = ['status', 'created']
     inlines = [SupportingInline]
+    actions = [make_active]
 
 
 class CollaboratorAdmin(admin.ModelAdmin):
     date_heirarchy = ['created']
+    list_filter = ['created']
 
 
 class SupportingMaterialAdmin(admin.ModelAdmin):
     date_heirarchy = ['created']
+    list_filter = ['status', 'created']
+    actions = [make_active]
 
 
 admin.site.register(Article, ArticleAdmin)
