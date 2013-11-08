78a488f54439052c6f59c6410ae60acae34979e4
codersquid <github@codersquid.com>
2013-11-08 01:46:18 -0600

    updated data importing scripts

--- companionpages/scripts/legacyload.py
+++ companionpages/scripts/legacyload.py
@@ -3,132 +3,106 @@
 import argparse
 import json
 import os
+from collections import defaultdict
 
 
 """
-This Reads a collaborators json file that was created by the
-https://github.com/codersquid/rmcscrape scraper and adds Collaborator records
-to db. You need to be in the same directory as manage.py to run this or
-fix the import depth.
+This reads a results json file that was created by the
+https://github.com/codersquid/rmcscrape scraper. This adds records as
+apropriate You need to have this in the same directory as manage.py to run
+this.
 
-Save collaborators before saving scrapes. I didn't write this to do everything
-once.
+NOTE: I inserted 3 "new_key" fields in the json that was output from the scraper.
+because the URLs from our prerelease database environment were given out to be
+put in documents. I needed to script those pk's by hand.
 
-collaborators is a dict keyed by strings that are names.
-"affiliation": optional string
-"country": optional string
-"coder": true or false
-"author": true or false
+* article keys from prerelease database environment, legacy keys
+* 36, 190
+* 54, 303
+* 49, 265
 
+results from the scrape have the following keys
 
-scrapes is a dict
-"coders": dictionary of coder collaborators keyed by name
+"coders": dictionary of coder dictionaries keyed by name
+"authors": dictionary of author dictionaries keyed by name
 "article_url": optional url string
 "names": list of strings of authors
 "legacy_id": int from old system
 "title": string
-"abstract": string
+"abstract": abstract of article
 "journal": string
-"explanatory_text": explanation string
+"explanatory_text": abstract of code and data
+
+coder and author dicts can have the following keys
+
+"affiliation": optional string (only was available in the scraped data for coders)
+"country": optional string (only was available in the scraped data for coders)
+"coder": true or false
+"author": true or false
+"last": last name
+"first": first name
 
 """
 
 
-def save_collaborators(collaborators):
-    for name, attributes in collaborators.items():
-        collaborator = models.Collaborator(
-            given_name=attributes.get('first', ''),
-            surname=attributes.get('last', ''),
-            coder=attributes.get('coder', False),
-            author=attributes.get('author', False),
-            affiliation=attributes.get('affiliation', ''),
-            country=attributes.get('country', ''),
-        )
-        collaborator.save()
-
-
-def save_articles(scrapes):
+def save_articles(scrapes, site_owner):
     for scrape in scrapes:
         coders = scrape.get('coders', {})
         article_url = scrape.get('article_url', '')
         authors = scrape.get('authors', {})
         legacy_id = scrape.get('legacy_id')
+        new_key = scrape.get('new_key', '')
         title = scrape.get('title', '')
         abstract = scrape.get('abstract', '')
         journal = scrape.get('journal', '')
         explanatory_text = scrape.get('explanatory_text', '')
 
-        all_firstnames = [authors[key]['first'] for key in authors if authors[key]['first'] is not None]
-        all_lastnames = [authors[key]['last'] for key in authors if authors[key]['last'] is not None]
-        coder_firstnames = [coders[key]['first'] for key in coders if coders[key]['first'] is not None]
-        coder_lastnames = [coders[key]['last'] for key in coders if coders[key]['last'] is not None]
-        all_firstnames.extend(coder_firstnames)
-        all_lastnames.extend(coder_lastnames)
+        # merge authors and coders.
+        authorship = defaultdict(dict)
+        for k in coders:
+            authorship[k].update(coders[k])
+        for k in authors:
+            authorship[k].update(authors[k])
 
-        coders = None
-        collaborators = None
-        if len(coder_lastnames) > 0:
-            coders = models.Collaborator.objects.filter(
-                given_name__in=coder_firstnames
-            ).filter(
-                surname__in=coder_lastnames
-            )
-            print coders
+        if new_key != '':
+            pk = int(new_key)
         else:
-            print 'no coders', legacy_id, title
-        if len(all_lastnames) > 0:
-            collaborators = models.Collaborator.objects.filter(
-                given_name__in=all_firstnames
-            ).filter(
-                surname__in=all_lastnames
-            )
-            print collaborators
-        else:
-            print 'no everyone', legacy_id, title
+            pk = int(legacy_id)
 
         article = models.Article(
+            id=pk,
+            site_owner=site_owner,
+            status=STATUS.active,
+            authorship=json.dumps(authorship),
             title=title,
-            abstract=abstract,
+            paper_abstract=abstract,
+            code_data_abstract=explanatory_text,
             journal=journal,
             legacy_id=legacy_id,
             article_url=article_url,
+            notes_for_staff="legacy",
         )
         article.save()
-
-        if collaborators is not None:
-            article.collaborators.add(*collaborators)
-        if coders is not None:
-            article.coders.add(*coders)
-
-        article.supportingmaterial_set.create(
-            name='materials supporting %s' % (title),
-            explanatory_text=explanatory_text,
-        )
 
 
 if __name__ == '__main__':
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "companionpages.settings")
-    from supportingmaterials import models
+    from compendia import models
+    from compendia.choices import STATUS
+    from users.models import User
 
     parser = argparse.ArgumentParser(description="""
-    This Reads a collaborators json file that was created by the
-    https://github.com/codersquid/rmcscrape scraper and adds
-    Collaborator records to db. You need to be in the same directory as manage.py to run this.
-
-    Save collaborators before saving scrapes. I didn't write this to do everything
-    at once.
+    This reads a scrape json file that was created by the
+    https://github.com/codersquid/rmcscrape scraper and adds article records to
+    the db. You need to be in the same directory as manage.py to run this.
     """)
 
-    parser.add_argument('--collaborators', type=file, help='a json file with collaborators.')
+    #parser.add_argument('--collaborators', type=file, help='a json file with collaborators.')
     parser.add_argument('--scrapes', type=file, help='a json file with scraped legacy data.')
     args = parser.parse_args()
-
-    if args.collaborators is not None:
-        print("loading collaborators...")
-        collaborators = json.load(args.collaborators)
-        save_collaborators(collaborators)
 
     if args.scrapes is not None:
         print("loading scrapes...")
         scrapes = json.load(args.scrapes)
-        save_articles(scrapes)
+        site_owner = User.objects.get(pk=1)
+        save_articles(scrapes, site_owner)
