d1a6658941443a3626fa6433938dead49f77100d
Jennifer Seiler <jese@aei.mpg.de>
2014-03-05 15:43:41 -0700

    giving up on the in-migrate fix
    it ought to be a separate script, anyway.

--- companionpages/compendia/migrations/0008_auto__add_field_article_month__add_field_article_year__add_field_artic.py
+++ companionpages/compendia/migrations/0008_auto__add_field_article_month__add_field_article_year__add_field_artic.py
@@ -49,15 +49,13 @@ class Migration(SchemaMigration):
                       keep_default=False)
 
         #Changing field 'Article.related_urls'
-        #db.execute('alter table compendia_article alter column related_urls type json using (related_urls::json)')
-        db.execute("ALTER TABLE compendia_article ALTER COLUMN related_urls TYPE json USING related_urls::json")
+        #db.execute("ALTER TABLE researchcompendia.compendia_article ALTER COLUMN related_urls TYPE json USING related_urls::json")
 
         db.alter_column(u'compendia_article', 'related_urls', 
                         self.gf('jsonfield.fields.JSONField')(default='{}'))
 
         # Changing field 'Article.authorship'
-        # db.execute('alter table compendia_article alter column authorship type json using (authorship::json)')
-        db.execute("ALTER TABLE compendia_article ALTER COLUMN authorship TYPE json USING authorship::json")
+        #db.execute("ALTER TABLE compendia_article ALTER COLUMN authorship TYPE json USING authorship::json")
         
         db.alter_column(u'compendia_article', 'authorship', 
                         self.gf('jsonfield.fields.JSONField')(default='{}'))
