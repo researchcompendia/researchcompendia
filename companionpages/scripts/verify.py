72cb9d1b3bc63165400af9fd67c456113643c1f8
codersquid <github@codersquid.com>
2014-04-03 15:25:43 -0500

    add an example in the comments

--- companionpages/scripts/verify.py
+++ companionpages/scripts/verify.py
@@ -3,8 +3,37 @@ import argparse, json
 from os.path import isfile
 import requests
 
+
 """
 Utility script for calling verification api from command line
+
+Here is an example of how you would call it from the command line. In this example, we
+are passing parameters to "Pizza Meetup Ordering Algorithm", which is compendia 11. $ is the
+command line.
+
+$ ./verify.py 11 --host=labs.researchcompendia.org --parameters=pizza.json
+
+This request results in this output:
+
+
+{
+  "message": "ok", 
+  "output_dir": "/tmp/compendiaSjNQzA/hellopizza/compendiaoutput", 
+  "output_files": [
+    {
+      "bytes": 101, 
+      "file": "pizza_order.json", 
+      "size": "101B"
+    }
+  ], 
+  "path_to_zipped_output": "/tmp/compendiaSjNQzA/hellopizza/compendiaoutput.zip", 
+  "requestid": "messageidnotusedyet", 
+  "status": 201, 
+  "stderr": "", 
+  "stdout": "{'attending': 33, 'pizzas': {'cheese': 3, 'meat': 3, 'vegan': 1, 'veg': 4}}\n\n    33 people will show up (guess)\n    3 cheese pizzas\n    3 meat pizzas\n    4 vegetarian pizzas\n    1 vegan pizzas\n    \n", 
+  "zipbytes": 199, 
+  "zipsize": "199B"
+}
 """
 
 def verify(parameters, compendia, host='localhost:8000'):
