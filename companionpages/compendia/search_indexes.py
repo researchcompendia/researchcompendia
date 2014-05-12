3344218c17c09de091f94188365ccc4c3e73616e
codersquid <github@codersquid.com>
2014-05-12 17:32:28 -0500

    add facets for search on compendia type and primary research field

--- companionpages/compendia/search_indexes.py
+++ companionpages/compendia/search_indexes.py
@@ -8,6 +8,8 @@ class ArticleIndex(indexes.SearchIndex, indexes.Indexable):
     authors_text = indexes.CharField(model_attr='authors_text')
     title = indexes.CharField(model_attr='title')
     code_data_abstract = indexes.CharField(model_attr='code_data_abstract')
+    compendium_type = indexes.CharField(model_attr='compendium_type', faceted=True)
+    primary_research_field = indexes.CharField(model_attr='primary_research_field', faceted=True)
 
     def get_model(self):
         return Article
