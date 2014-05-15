a53d71402f34ec87b49c782e74e2ebfaec2718ee
codersquid <github@codersquid.com>
2014-05-15 15:43:06 -0500

    pretty print facets

--- companionpages/home/templatetags/home_tags.py
+++ companionpages/home/templatetags/home_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.conf import settings
 from django.utils.safestring import mark_safe
 from django.template.defaultfilters import stringfilter
 
@@ -13,3 +14,9 @@ register = template.Library()
 def md(text):
     html = markdown.markdown(text, safe_mode='replace', output_format='html5')
     return mark_safe(html)
+
+
+@register.filter
+def lookup(dictionary, key):
+    # see https://code.djangoproject.com/ticket/3371
+    return dictionary.get(key, settings.TEMPLATE_STRING_IF_INVALID)
