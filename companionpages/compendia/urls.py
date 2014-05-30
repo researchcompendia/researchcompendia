71614463c4625eddd3dd4b6df4ced28712ec0f63
codersquid <github@codersquid.com>
2014-05-30 14:24:20 -0500

    handle 404 situations for table of contents and browse views

--- companionpages/compendia/urls.py
+++ companionpages/compendia/urls.py
@@ -6,7 +6,7 @@ urlpatterns = patterns(
     '',
     url(r'^$', views.ArticleListView.as_view(), name='list'),
     url(r'^browse/(?P<slug>.+)/$', views.ArticleTypeListView.as_view(), name='browse'),
-    url(r'^b/$', views.ArticleBrowseView.as_view(), name='b'),
+    # url(r'^b/$', views.ArticleBrowseView.as_view(), name='b'),
     url(r'^toc/$', views.TableOfContentsView.as_view(), name='toc'),
     url(r'^create/$', views.ArticleCreateView.as_view(), name='create'),
     url(r'^(?P<year>\d{4})\.(?P<pk>\d+)/$', views.ArticleYearView.as_view(), name='year_detail'),
